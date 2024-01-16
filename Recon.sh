aaabcd056ebdfde168841fbb5d899c35fce7b090
Jaikumar3 <71895479+Jaikumar3@users.noreply.github.com>
2024-01-16 22:28:06 +0530

    Update Recon.sh

--- Recon.sh
+++ Recon.sh
@@ -6,7 +6,7 @@
 # Description: This script automates various security testing tasks using a set of tools.
 #
 
-set -e
+#set -e
 
 # Tools Used:
 # - brutespray: Password spraying tool for various services
@@ -27,9 +27,6 @@ set -e
 # Note: Ensure that Python3 and nmap are already installed on your system.
 # If not, you can install Python3 using your system's package manager, and nmap using: sudo apt-get install nmap
 
-#Tool paths
-Path=~/tools/smbghost/
-
 # Set variables
 IP="$1"
 subnet_range=$(echo "$IP" | cut -d. -f1-3)
@@ -49,10 +46,11 @@ echo "Looking for web-related vulnerabilities..."
 echo "$IP" | httpx -p '80,81,82,90,443,444,446,447,448,449,450,451,1947,5000,5800,8000,8443,8080,8081,8089,8888,1072,1556,1947,2068,2560,3128,3172,3387,3580,3582,3652,4343,4480,5000, 
 5800,5900,5985,5986,8001,8030,8082,8083,8088,8089,8090,8443,8444,8445,8910,9001,9090,9091,20000' --title | tee $results_dir/httpxresults
 
+echo "Using nmap results grabing web service"
 cat nmap_tcp_scan.xml | nmapurls | tee nmap_webservice_results
 
 echo "Running Nuclei scanning..."
-cat $results_dir/httpxresults | nuclei | tee $results_dir/nucleiresults
+#cat $results_dir/httpxresults | nuclei | tee $results_dir/nucleiresults
 
 echo "Scanning for SMBv1, signing, and Windows details..."
 cme smb "$IP" | tee $results_dir/smb_results
@@ -80,14 +78,17 @@ rpcclient -U '' -N "$IP" -c enumdomains 2>&1 | tee -a $results_dir/rpc-check.txt
 rpcclient -U '' -N "$IP" -c enumdomgroups 2>&1 | tee -a $results_dir/rpc-check.txt
 
 echo "Running password spraying..."
-python3 brutespray.py --file $results_dir/ -U /usr/share/wordlist/user.txt -P /usr/share/wordlist/pass.txt -c -o password_spray_results
+#python3 brutespray.py --file $results_dir/ -U /usr/share/wordlist/user.txt -P /usr/share/wordlist/pass.txt -c -o password_spray_results
 
 echo "Checking for BlueKeep vulnerability..."
-python3 bluekeep.py "$IP"
+#python3 bluekeep.py "$IP"
 
 echo "Checking for SMBGhost vulnerability..."
-python3 SMBGhost/scanner.py "$IP"
+#python3 SMBGhost/scanner.py "$IP"
 
-echo "Script execution completed. Results stored in: $results_dir"
-trap - ERR
-cleanup
+echo "texttohtml"
+python3 /users/jai/text2html.py -i $results_dir -o $results_dir/results.html
+
+#echo "Script execution completed. Results stored in: $results_dir"
+#trap - ERR
+#cleanup
